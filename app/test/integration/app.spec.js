9dab5dcf975850b4a38c5e4bb7c0f33ca706e9f2
DiegoBulhoes <diegobulhoesm@gmail.com>
2019-05-21 15:15:11 -0400

    Update dependencies

--- app/test/integration/app.spec.js
+++ app/test/integration/app.spec.js
@@ -1,3 +1,8 @@
+import supertest from 'supertest';
+import app from '../../app';
+
+const request = supertest(app);
+
 describe('GET /users', () => {
   it('should return a user', (done) => {
     request.get('/user').expect(200, done);
